db07b9761c3584e8575afae2064c553b016eea4e
CorianderSeb <106112163+CorianderSeb@users.noreply.github.com>
2022-10-13 03:46:51 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React from "react";
 import bootstrap from "bootstrap";
 import Weather from "./Weather"
 import './App.css';
@@ -6,14 +6,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className= "container">
-      <header className="App-header">
-      <h1>
-        Weather App</h1> 
-      <Weather />
-      </header>
-      <footer>This project was coded by <a href="https://www.sebastiancseithel.com" target="_blank" rel="noopener nonreferrer">Sebastian Cory Seithel</a> and is open sourced on <a href="">GitHub</a>. </footer>
-    </div>
+      <div className="container">
+        <header className="App-header"></header>
+        <div><Weather city="Dallas"/></div>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://sebastiancseithel.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sebastian Cory Seithel
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/CorianderSeb/react-weather-forecast-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          .{" "}
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,33 @@
-import React, {useState} from "react";
+import React from "react";
+import "./Weather.css";
+
 
 export default function Weather () {
     return (
-        <div> Hello from Weather</div>
-    )
+      <div className="Weather">
+          <h1> Dallas</h1>
+          <ul>
+            <li>Wednesday 01:00</li>
+            <li>Sunny</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>
+                        Precipitation: 0%
+                    </li>
+                    <li>
+                        Humidity: 40%
+                    </li>
+                    <li>
+                        Wind: 7mph
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+    );
 }
