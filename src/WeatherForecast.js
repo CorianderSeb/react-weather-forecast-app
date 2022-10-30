67849bc6e498455ab467dd97b2d2bab9523f7db7
CorianderSeb <106112163+CorianderSeb@users.noreply.github.com>
2022-10-30 05:57:20 -0400

    Used use change to update forecast and finalized CSS

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
     let [forecastReady, setForecastReady] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+setForecastReady(false);
+    }, [props.data.city]);
 
     function handleResponse (response) {
     setForecast(response.data.daily);
@@ -17,9 +21,15 @@ export default function WeatherForecast(props) {
 return (
   <div className="WeatherForecast">
     <div className="row">
-      <div className="col">
-        <WeatherForecastDay data={forecast[0]} />
-    </div>
+        {forecast.map(function(dailyforecast,index){
+            if (index < 6){
+        return (
+            <div className="col">
+                <WeatherForecastDay data={dailyforecast} />
+            </div>
+        ); 
+            }   
+        })}
   </div>
   </div>
 );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,30 +1,30 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherTemperature from "./WeatherTemperature"
+import "./WeatherInfo.css";
 
 
 export default function WeatherInfo (props) {
   return (
-      <div className="WeatherInfo">
-        <h1> {props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
-          <li className="text-capitalize">{props.data.condition}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img src={props.data.icon} alt="{props.data.condition}" />
-            <WeatherTemperature farenheit={props.data.temperature} />
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} mph</li>
-            </ul>
-          </div>
+    <div className="WeatherInfo">
+      <h1> {props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize condition">{props.data.condition}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src={props.data.icon} alt="{props.data.condition}" />
+          <span className="temperature">{Math.round(props.data.temperature)}</span><span className="unit">°F</span>
+        </div>
+        <div className="col-6 humidity-temperature">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} mph</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
